6f8ea051bbf41f5859619f89073dd6ca9e0eeff2
LUZWIN <luzwinzc@gmail.com>
2025-05-31 03:37:24 -0500

    Se sube el Historial de validaciones

---
+++ Backend.PayFlow.API/Controllers/HistValidController.cs
@@ -0,0 +1,56 @@
+﻿using Backend.PayFlow.DOMAIN.Core.Entities;
+using Backend.PayFlow.DOMAIN.Infrastructure.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend.PayFlow.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HistValidController : ControllerBase
+    {
+        private readonly PayFlowDbContext _context;
+
+        public HistValidController(PayFlowDbContext context)
+        {
+            _context = context;
+        }
+
+
+        // GET: api/HistValid
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<HistorialValidaciones>>> GetAllHistValid()
+        {
+            return await _context.HistorialValidaciones.ToListAsync();
+        }
+
+
+        // GET: api/HistValid/Manual
+        [HttpGet("{TipVal}")]
+        public async Task<ActionResult<HistorialValidaciones>> GetHistValidByTipo(string TipoVal)
+        {
+            var hist_Valid = await _context.HistorialValidaciones.Where(c => c.TipoValidacion == TipoVal).FirstOrDefaultAsync();
+
+            if (hist_Valid == null)
+            {
+                return NotFound();
+            }
+
+            return hist_Valid;
+        }
+
+
+        // POST: api/HistValid
+        [HttpPost]
+        public async Task<ActionResult<Roles>> PostHistValid(HistorialValidaciones hist_Valid)
+        {
+            _context.HistorialValidaciones.Add(hist_Valid);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetAllHistValid", new { id = hist_Valid.IdHistorial }, hist_Valid);
+        }
+
+
+    }
+}
