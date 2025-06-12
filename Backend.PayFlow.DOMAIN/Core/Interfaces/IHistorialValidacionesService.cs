48ed72bc9838a532b93b8d2dc6479d8ffbd72843
MiguelHR17 <miguel.ehr1@gmail.com>
2025-06-12 15:36:55 -0500

    Historial Validaciones hasta DTOs

---
+++ Backend.PayFlow.DOMAIN/Core/Interfaces/IHistorialValidacionesService.cs
@@ -0,0 +1,13 @@
+﻿using Backend.PayFlow.DOMAIN.Core.Entities;
+
+namespace Backend.PayFlow.DOMAIN.Core.Interfaces
+{
+    public interface IHistorialValidacionesService
+    {
+        bool AddValidation(HistorialValidaciones validation);
+        bool DeleteValidation(int id);
+        Task<List<HistorialValidaciones>> GetAllValidationsAsync();
+        Task<HistorialValidaciones> GetValidationByIdAsync(int id);
+        bool UpdateValidation(HistorialValidaciones validation);
+    }
+}
