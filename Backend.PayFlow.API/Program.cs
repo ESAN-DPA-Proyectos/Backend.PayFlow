88f3e6f5ed86e88147e42add428e63aa872f8d91
willtl2000 <wilber.ticona@outlook.com>
2025-06-14 06:05:58 -0500

    Avance de Entidad Fondos

--- Backend.PayFlow.API/Program.cs
+++ Backend.PayFlow.API/Program.cs
@@ -29,6 +29,8 @@ builder.Services.AddScoped<ISeguimientoTransaccionService, SeguimientoTransaccio
 builder.Services.AddTransient<IHistorialValidacionesRepository, HistorialValidacionesRepository>();
 builder.Services.AddScoped<IHistorialValidacionesService, HistorialValidacionesService>();
 
+builder.Services.AddTransient<IFondosRepository, FondosRepository>();
+
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
